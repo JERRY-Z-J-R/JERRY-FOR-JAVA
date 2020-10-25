46eaae99aa10a2c46773ffc4adb5b819d330860d
JERRY-Z-J-R <www.zjr1846334075@foxmail.com>
2020-10-26 01:02:00 +0800

    add One.java

---
+++ One.java
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-26 01:01:00
+ * @LastEditTime: 2020-10-26 01:01:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \undefinede:\MyCode\MyGit\JERRY-For-JAVA\One.java
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
