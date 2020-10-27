d4749efb4942d76009ee60c31629a674de6ae52c
JERRY-Z-J-R <www.zjr1846334075@foxmail.com>
2020-10-27 12:49:30 +0800

    first update JerryGame.java

--- JerryGuessNumberGame/JerryGame.java
+++ JerryGuessNumberGame/JerryGame.java
@@ -1,14 +1,25 @@
 /*
-* 项目：具有用户登录功能的猜数游戏
+* 项目：具有用户登录功能且集合了控制台与图形用户界面的猜数游戏
 * 姓名：周吉瑞 20190521340
-* 时间：2020/10/26
+* 时间：2020/10/27
  */
 
 package cxtc.jk03;
 import java.util.Scanner;
 import java.util.Random;
 
-public class JerryGame {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class JerryGame extends JFrame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -39,8 +50,28 @@ public class JerryGame {
                     System.out.println("****************************");
                 }//for
                 System.out.println("******成功登录，开始游戏!******");
-                guessNumberGame();  //调用游戏函数
-                break;
+                System.out.println("********请选择游戏界面*********");
+                System.out.println("1.控制台界面");
+                System.out.println("2.图形用户界面");
+                System.out.print("请输入界面序号：");
+                int n = scanner.nextInt();
+                if (n == 1) {
+                    guessNumberGame();  //调用游戏函数
+                    break;
+                } else if (n == 2) {
+                    JerryGame theGame = new JerryGame();
+                    theGame.newGame();
+                    theGame.setSize(new Dimension(450, 300));
+                    theGame.setVisible(true);
+                    break;
+                } else {
+                    System.out.println("输入错误，默认选择图形用户界面");
+                    JerryGame theGame = new JerryGame();
+                    theGame.newGame();
+                    theGame.setSize(new Dimension(450, 300));
+                    theGame.setVisible(true);
+                    break;
+                }
             }//if
             if (i != 3) {
                 System.out.println("用户名密码错误，还剩" + (3 - i) + "次机会");
@@ -86,7 +117,10 @@ public class JerryGame {
 
     }//login
 
+    //***************************************************************
+    //图形用户界面
     //游戏功能函数
+
     public static void guessNumberGame() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("*******Game  Start!!!*******");
@@ -97,6 +131,11 @@ public class JerryGame {
         int randNum = rand.nextInt(100) + 1;
 
         while (true) {
+            if (count >= 7) {
+                System.out.println("次数已经用完，游戏结束！");
+                break;
+            }
+
             System.out.print("请猜一个1~100的整数：");
             int userNum = scanner.nextInt();
             count++;
@@ -107,10 +146,122 @@ public class JerryGame {
                 break;
             } else if (userNum < randNum) {
                 System.out.println("亲爱的，猜小啦！");
+                System.out.println("已猜测" + count + "次");
+                System.out.println("还剩下" + (7 - count) + "次");
             } else {
                 System.out.println("小可爱，猜大啦！");
+                System.out.println("还剩下" + (7 - count) + "次");
             }//else
         }//while
     }//guessNumberGame
 
+    //***************************************************************
+    //图形用户界面
+
+        private JTextField txtGuess;
+        private JLabel lblOutput;
+        private JButton btnGuess;
+        private JButton btnPlayAgain;
+        private int theNumber;
+        private int numberOfGames;
+
+        public void checkGuess() {
+            String guessText = txtGuess.getText();
+            String message = "";
+            try {
+                int guess = Integer.parseInt(guessText);
+                if (guess < theNumber) {
+                    message = guess + "太小了！再猜一次！还剩" + (numberOfGames - 1) + "次机会！";
+                } else if (guess > theNumber) {
+                    message = guess + "太大了！再猜一次！还剩" + (numberOfGames - 1) + "次机会！";
+                } else {
+                    message = guess + "就是那个数字！你猜对啦！让我们再玩儿一遍吧！";
+                    btnPlayAgain.setVisible(true);
+                    gameOver();
+                    return;
+                }
+
+                numberOfGames--;
+
+                if (numberOfGames <= 0) {
+                    message = guess + "不对，没有机会了，正确答案是：" + theNumber;
+                    btnPlayAgain.setVisible(true);
+                    gameOver();
+                }
+
+            } catch (Exception e) {
+                message = "请输入1-100之间的整数！还剩" + numberOfGames + "次机会！";
+            } finally {
+                lblOutput.setText(message);
+                txtGuess.requestFocus();
+                txtGuess.selectAll();
+            }
+        }
+
+        public void newGame() {
+            theNumber = (int) (Math.random() * 100 + 1);
+            numberOfGames = 7;  //第一次执行了一次初始化值为6
+            btnGuess.setEnabled(true);
+            txtGuess.setEnabled(true);
+        }
+
+        //游戏结束使猜测按钮和输入框不可选中
+        public void gameOver() {
+            btnGuess.setEnabled(false);
+            txtGuess.setEnabled(false);
+        }
+
+        public JerryGame() {
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setTitle("JERRY'S 猜数游戏");
+            getContentPane().setLayout(null);
+
+            JLabel lblNewLabel = new JLabel("Game Start!");
+            lblNewLabel.setFont(new Font("宋体", Font.BOLD, 15));
+            lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            lblNewLabel.setBounds(159, 27, 116, 18);
+            getContentPane().add(lblNewLabel);
+
+            JLabel lblNewLabel_1 = new JLabel("\u8BF7\u731C\u4E00\u4E2A1-100\u4E4B\u95F4\u7684\u6570\u5B57\uFF1A");
+            lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+            lblNewLabel_1.setBounds(26, 72, 213, 15);
+            getContentPane().add(lblNewLabel_1);
+
+            txtGuess = new JTextField();
+            txtGuess.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent arg0) {
+                    checkGuess();
+                }
+            });
+            txtGuess.setBounds(250, 69, 66, 21);
+            getContentPane().add(txtGuess);
+            txtGuess.setColumns(10);
+
+            btnGuess = new JButton("猜一下！");
+            btnGuess.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent arg0) {
+                    checkGuess();
+                }
+            });
+            btnGuess.setBounds(170, 114, 93, 23);
+            getContentPane().add(btnGuess);
+
+            lblOutput = new JLabel("请在上面输入框中输入数字并点击猜一下！现在你有7次机会");
+            lblOutput.setHorizontalAlignment(SwingConstants.CENTER);
+            lblOutput.setBounds(50, 214, 350, 15);
+            getContentPane().add(lblOutput);
+
+            btnPlayAgain = new JButton("再玩一次！");
+            btnPlayAgain.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    newGame();
+                    txtGuess.setText("");
+                    lblOutput.setText("请在上面输入框中输入数字并点击猜一下！你有7次机会！");
+                    btnPlayAgain.setVisible(false);
+                }
+            });
+            btnPlayAgain.setBounds(164, 164, 105, 23);
+            btnPlayAgain.setVisible(false);
+            getContentPane().add(btnPlayAgain);
+        }
 }
