f7b846f9cbb29cb8ef302d73e1f5a26572dd43cb
JERRY-Z-J-R <www.zjr1846334075@foxmail.com>
2020-12-13 00:17:16 +0800

    add .

--- Project04/Test05.java
+++
@@ -1,9 +0,0 @@
-public class Test05 {
-    public static void main(String[] args) {
-        int sum = 1;
-        for (int i = 1; i <= 9; i++) {
-            sum = (sum + 1) * 2;
-        }
-        System.out.println(sum);
-    }
-}
